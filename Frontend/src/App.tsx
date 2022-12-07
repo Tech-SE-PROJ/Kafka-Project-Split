760855f5580a7910f2008f8eb58a2237054143c6
zpeterson251 <zpeterson251@gmail.com>
2022-12-07 10:50:11 -0500

    Working Table

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,26 +1,55 @@
-import React, {useEffect, Component} from 'react';
+import { render } from '@testing-library/react';
+import React, {useEffect} from 'react';
 import './App.css';
-import ButtonLoader from "./FetchButton";
-import QueryBackend from "./QueryBackend";
+import Table from './Table';
 
-
-
-function App() {
-  const comp = new Component(QueryBackend)
-  useEffect(() => {
-    comp.()
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-      <div className="buttonIn">
-        <input type="text" id="enter" placeholder="Enter a topic"/>
-        <ButtonLoader />
-      </div>
-      </header>
-    </div>
-  );
+const QueryBackend = async () => {
+  const response = await fetch("http://127.0.0.1:3000/ksql", {mode:'cors'})
+  const backendResponse = await response.json();
+  console.log(backendResponse)
 }
+
+class App extends React.Component {
+  // useEffect(() => {
+  //   QueryBackend()
+  // }, []);
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="buttonIn">
+            <input type="text" id="enter" placeholder="Enter a topic"/>
+            <button id="query" onClick={QueryBackend}>Query</button>
+          </div>
+      <div>
+          <Table/>
+      </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+
+// function App() {
+//   useEffect(() => {
+//     QueryBackend()
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <div className="buttonIn">
+//           <input type="text" id="enter" placeholder="Enter a topic"/>
+//           <button id="query" onClick={QueryBackend}>Query</button>
+//         </div>
+//     <div>
+//         <Table/>
+//     </div>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
